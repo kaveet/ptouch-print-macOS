f27ef9e139981be322cc2ff50f762d94ce6ed484
David Phillip Oster <DavidPhillipOster@gmail.com>
2023-12-17 18:16:59 -0800

    Add missing files

---
+++ ptouch-print/gdMac.m
@@ -0,0 +1,201 @@
+//  gdMac.m
+//  ptouch-print
+//
+//  Created by David Phillip Oster on 12/16/23.
+//
+
+#import <AppKit/AppKit.h>
+
+#import <CoreGraphics/CoreGraphics.h>
+
+#import "gd.h"
+
+static NSData *sDashStyle = nil;
+
+int gdFTUseFontConfig(int flag) {
+ return GD_TRUE;
+}
+
+int gdImageBlue(gdImage *im, int index) {
+ return gdImageGetPixel(im, index, 0);
+}
+
+// Since we are working in grayscale for black and white, jjust use the average.
+int gdImageColorAllocate(gdImage *im, int red, int green, int blue) {
+  return (red + green + blue)/3;
+}
+
+void gdImageCopy(gdImage * dst, gdImage * src, int dstX, int dstY, int srcX, int srcY, int w, int h) {
+  CGRect srcR = CGRectMake(srcX, srcY, w, h);
+  CGImageRef srcImage = CGBitmapContextCreateImage(src);
+  CGImageRef srcRectImage = CGImageCreateWithImageInRect(srcImage, srcR);
+  CGRect destR = CGRectMake(dstX, dstY, w, h);
+  CGContextDrawImage(dst, destR, srcRectImage);
+  CGImageRelease(srcRectImage);
+  CGImageRelease(srcImage);
+}
+
+gdImage *gdImageCreateFromPng(FILE *inFile) {
+  fseek(inFile, 0, SEEK_END);
+  NSInteger length = ftell(inFile);
+  rewind(inFile);
+  NSMutableData *data = [[NSMutableData alloc] initWithLength:length];
+  if (data && 1 == fread(data.mutableBytes, data.length, 1, inFile)) {
+    NSImage *image = [[NSImage alloc] initWithData:data];
+    if (image) {
+      CGRect r = CGRectMake(0, 0, image.size.width, image.size.height);
+      CGImageRef ref = [image CGImageForProposedRect:&r context:nil hints:nil];
+      if (ref) {
+        gdImage *result = gdImageCreatePalette(image.size.width, image.size.height);
+        if (result) {
+          CGContextDrawImage(result, r, ref);
+          return result;
+        }
+      }
+    }
+  }
+  return NULL;
+}
+
+gdImage *gdImageCreatePalette(int x, int y) {
+  const int bitsPerPixel = 8;
+  static CGColorSpaceRef gray = NULL;
+  if(NULL == gray){
+    gray = CGColorSpaceCreateWithName(kCGColorSpaceGenericGray);
+  }
+  uint32_t info = kCGImageByteOrderDefault | kCGImageAlphaNone;
+  CGContextRef result = CGBitmapContextCreate(NULL, x, y, bitsPerPixel, (x*bitsPerPixel + 7)/8, gray, info);
+  if (result) {
+    uint8_t *p = (uint8_t *)CGBitmapContextGetData(result);
+    int rowBytes = (int)CGBitmapContextGetBytesPerRow(result);
+    for(int j = 0;j < y; ++j) {
+      memset(p, 255, rowBytes);
+      p += rowBytes;
+    }
+  }
+  return result;
+}
+
+void gdImageDestroy(gdImage *im) {
+  CGContextRelease(im);
+}
+
+int gdImageGetPixel(gdImage *im, int x, int y) {
+  uint8_t *base = (uint8_t *)CGBitmapContextGetData(im);
+  return base[x + y*CGBitmapContextGetBytesPerRow(im)];
+}
+
+int gdImageGreen(gdImage *im, int index) {
+ return gdImageGetPixel(im, index, 0);
+}
+
+void gdImageLine(gdImage *im, int x1, int y1, int x2, int y2, int color) {
+  CGFloat colors[3];
+  CGContextBeginPath(im);
+  if (color == gdStyled) {
+    if (sDashStyle) {
+      colors[0] = 0;    // black
+      colors[1] = 1.0;  // opaque
+      // phase=1, so we start with length of transparent.
+      CGContextSetLineDash(im, 1, sDashStyle.bytes, sDashStyle.length/sizeof(CGFloat));
+    }
+  } else {
+    CGContextSetLineDash(im, 0, NULL, 0);
+    colors[0] = color / 255.0;
+    colors[1] = 1.0;
+  }
+  CGContextSetStrokeColor(im, colors);
+  // since pen is one pixel wide, offset in X to fill pixel with color
+  CGFloat offset = (x1 == x2) ? -0.5 : 0;
+  CGContextMoveToPoint(im, x1 + offset, y1);
+  CGContextAddLineToPoint(im, x2 + offset, y2);
+  CGContextStrokePath(im);
+}
+
+int gdImagePng(gdImage *im, FILE *outFile) {
+  CGSize cgImageSize;
+  cgImageSize.width = CGBitmapContextGetWidth(im);
+  cgImageSize.height = CGBitmapContextGetHeight(im);
+  CGImageRef cgImage = CGBitmapContextCreateImage(im);
+  if (cgImage) {
+    NSImage *image = [[NSImage alloc] initWithCGImage:cgImage size:cgImageSize];
+    if (image) {
+      NSData *imageData = [image TIFFRepresentation];
+      if (imageData) {
+        NSBitmapImageRep *imageRep = [NSBitmapImageRep imageRepWithData:imageData];
+        NSDictionary *imageProps = @{NSImageCompressionFactor: @(1.0)};
+        imageData = [imageRep representationUsingType:NSBitmapImageFileTypePNG properties:imageProps];
+        if (imageData) {
+          return 1 != fwrite(imageData.bytes, imageData.length, 1, outFile);
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+int gdImageRed(gdImage *im, int index) {
+ return gdImageGetPixel(im, index, 0);
+}
+
+void gdImageSetStyle(gdImage *im, int *style, int styleLength){
+  if (styleLength == 6) {
+    int i = 0;
+    Boolean isDash = YES;
+    for (; i < 3 && isDash;++i) {
+      isDash |= style[i] == gdTransparent;
+    }
+    for (; i < 6 && isDash;++i) {
+      isDash |= style[i] == 0;
+    }
+    if (isDash) {
+      // input is specified in pixels, one entry per pixel. transparent first. CGContext wants line segments lengths, drawing first.
+      CGFloat dash[] = { 3, 3 };
+      sDashStyle = [NSData dataWithBytes:dash length:sizeof(CGFloat)];
+    }
+  }
+
+}
+
+char *gdImageStringFT(gdImage *im, int *brect, int fg, char *fontname, double ptsize, double angle, int x, int y, char *string) {
+  @autoreleasepool {
+    NSFont *font = [NSFont fontWithName:[NSString stringWithUTF8String:fontname] size:ptsize];
+    if (nil == font) {
+      return "Can’t get font";
+    }
+    NSString *nsString = [NSString stringWithUTF8String:string];
+    if (nil == nsString) {
+      return "Can’t convert to UTF8";
+    }
+    NSDictionary *dict = @{NSFontAttributeName: font, NSForegroundColorAttributeName : [NSColor colorWithWhite:fg/255.0 alpha:1]};
+    if (im) {
+      NSGraphicsContext *context = [NSGraphicsContext graphicsContextWithCGContext:im flipped:NO];
+      NSGraphicsContext *preserve = [NSGraphicsContext currentContext];
+      [NSGraphicsContext setCurrentContext:context];
+      [nsString drawAtPoint:CGPointMake(x, gdImageSY(im) - y) withAttributes:dict];
+      [NSGraphicsContext setCurrentContext:preserve];
+    }
+    if (brect) {
+      CGRect r = [nsString boundingRectWithSize:CGSizeMake(100000, 100000) options:0 attributes:dict];
+      brect[0] = r.origin.x + x; // lower left
+      brect[1] = r.origin.y + y + r.size.height;
+      brect[2] = r.origin.x + x + r.size.width; // lower right
+      brect[3] = r.origin.y + y + r.size.height;
+
+      brect[4] = r.origin.x + x;  // upper left
+      brect[5] = r.origin.y + y;
+      brect[6] = r.origin.x + x + r.size.width; // upper right
+      brect[7] = r.origin.y + y;
+    }
+    return NULL;
+  }
+}
+
+int gdImageSX(gdImage *im) {
+ return (int) CGBitmapContextGetWidth(im);
+}
+
+int gdImageSY(gdImage *im) {
+ return (int) CGBitmapContextGetHeight(im);
+}
+
---
+++ ptouch-print/include/gd.h
@@ -0,0 +1,59 @@
+//  gd.h
+//  ptouch-print
+//
+//  Created by David Phillip Oster on 12/16/23.
+//
+
+#ifndef gd_h
+#define gd_h
+
+enum {
+  GD_FALSE,
+  GD_TRUE
+};
+
+static const char *VERSION = "0.01";
+
+static const int gdTransparent = 256;
+static const int gdStyled = 257;
+
+typedef struct CGContext CGImage;
+typedef CGImage gdImage;
+
+int gdFTUseFontConfig(int flag);
+int gdImageBlue(gdImage *im, int index);
+int gdImageColorAllocate(gdImage *im, int red, int green, int blue);
+void gdImageCopy(gdImage * dst, gdImage * src, int dstX, int dstY, int srcX, int srcY, int w, int h);
+gdImage *gdImageCreateFromPng(FILE *inFile);
+gdImage *gdImageCreatePalette(int x, int y);
+void gdImageDestroy(gdImage *im);
+int gdImageGetPixel(gdImage *im, int x, int y);
+int gdImageGreen(gdImage *im, int index);
+void gdImageLine(gdImage *im, int x1, int y1, int x2, int y2, int color);
+int gdImagePng(gdImage *im,FILE *outFile);
+int gdImageRed(gdImage *im, int index);
+
+/// @param im	The image to draw onto.
+/// @param style array of pixel colors
+/// @param styleLength number of items in style array
+void gdImageSetStyle(gdImage *im, int *style, int styleLength);
+
+/// @param im	The image to draw onto.
+/// @param brect	The bounding rectangle as array of 8 integers where each pair represents the x- and y-coordinate of a point.  The points specify the lower left, lower right, upper right and upper left corner.
+/// @param fg	The font color.
+/// @param fontname	The font
+/// @param ptsize	The height of the font in typographical points (pt).
+/// @param angle	The angle in radian to rotate the font counter-clockwise.
+/// @param x	The x-coordinate of the basepoint (roughly the lower left corner) of the first letter.
+/// @param y	The y-coordinate of the basepoint (roughly the lower left corner) of the first letter.
+/// @param string	The string to render.
+/// @return nil on success,  errorString on failure
+char *gdImageStringFT(gdImage *im, int *brect, int fg, char *fontname, double ptsize, double angle, int x, int y, char *string);
+
+/// @return width in pixels of image
+int gdImageSX(gdImage *im);
+
+/// @return height in pixels of image
+int gdImageSY(gdImage *im);
+
+#endif /* gd_h */
